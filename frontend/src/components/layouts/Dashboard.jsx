030789ed9be05244da4538b8061996783e416c7b
chimaobi <okehchimaobi@gmail.com>
2024-11-30 07:57:27 +0100

    made some updates and replaced flask with php

---
+++ frontend/src/components/layouts/Dashboard.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
+function App() {
+  const navigate = useNavigate();
+
+    return (
+    <div className='dashboard-container'>
+      <h1>This is my PHP backend</h1>
+    </div>
+  );
+}
+
+export default App;
+    
