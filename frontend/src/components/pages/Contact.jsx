740ee0faf65a936f618ce900fd732498251b6b6f
Iceking <okehchimaobi@gmail.com>
2025-04-03 02:16:41 +0100

    update and deploy on vercel

--- frontend/src/components/pages/Contact.jsx
+++ frontend/src/components/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../stylesheets/pagestyles/contact.css";
+import "/src/stylesheets/pagestyles/contact.css";
 import Header from "../Header";
 import Footer from "../Footer";
 import Copyright from "../Copyright";
